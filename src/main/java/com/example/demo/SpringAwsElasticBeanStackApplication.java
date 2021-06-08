8c574156197cdfc37d234288955c38fd6f2a990c
miclejain19 <michellejain19@gmail.com>
2021-06-08 06:57:43 +0530

    initialgt

---
+++ src/main/java/com/example/demo/SpringAwsElasticBeanStackApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringAwsElasticBeanStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAwsElasticBeanStackApplication.class, args);
+	}
+
+}
+
+@RestController
+class Controller{
+
+	@GetMapping("/getName")
+	public String getName(){
+		return "Hello Ankit";
+	}
+}
